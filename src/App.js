93c2efa1beaed92675cca0d8bbb3d93b63ff0186
Bhashkar-Abhishek <126654468+Bhashkar-Abhishek@users.noreply.github.com>
2023-04-20 23:33:14 +0530

    uptated Protected routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import Home from './pages/home/Home';
-import AboutUs from './pages/aboutUs/AboutUs';
 import Classes from './pages/program/Program';
 import Login from './pages/login/Login';
-// import Footer from './pages/Footer';
 import Registation from './pages/registration/Registration';
 import Training from './pages/training/Training';
-import Pricing from './pages/pricing/Pricing';
+import Pricing from './pages/mui_pricing/Pricing';
+import AboutUs from './pages/about/AboutUs';
+import ProtectsRoutes from './services/ProtectsRoutes';
+
 
 function App() {
   return (
@@ -19,11 +20,19 @@ function App() {
       <Route path='/programs' element={<Classes/>} />
       <Route path='/training' element={<Training/>} />
       <Route path='/pricing' element={<Pricing/>} />
-      <Route path='/login' element={<Login/>} />
+      
+
+      {/* Protecting Routind =>> */}
+
+      <Route path='/registation' element={<ProtectsRoutes/>}>
       <Route path='/registation' element={<Registation/>} />
+      </Route>
+      <Route path='/login' element={<ProtectsRoutes/>}>
+      <Route path='/login' element={<Login/>} />
+      </Route>
       
     </Routes>
-    {/* <Footer/> */}
+    
     </BrowserRouter>
   );
 }
