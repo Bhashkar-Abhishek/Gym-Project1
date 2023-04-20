cbda4cf7fb85975b816d8092a714b93df5faf9f3
Bhashkar-Abhishek <126654468+Bhashkar-Abhishek@users.noreply.github.com>
2023-04-20 23:50:00 +0530

    Updated

--- src/pages/training/Training.js
+++ src/pages/training/Training.js
@@ -3,22 +3,24 @@ import { FaLessThan, FaGreaterThan } from 'react-icons/fa';
 import Navbar from "../navbar/Navbar"
 import Style from "./Training.module.css"
 import Footer from '../footer/Footer';
-
 const Training = () => {
   const photos = [
     {
       url: 'https://img2.wallspic.com/previews/1/0/0/1/31001/31001-bodybuilder-arm-strength_training-male-fitness_centre-x750.jpg',
       name: 'Strngth Training'
-    },{
-      url: 'https://images.pexels.com/photos/1954524/pexels-photo-1954524.jpeg?cs=srgb&dl=pexels-william-choquette-1954524.jpg&fm=jpg',
-      name: 'Stamina Training'
-    },{
-      url: 'https://wallpapercave.com/wp/wp11292746.jpg',
-      name: 'Octagon'
-    },{
-      url: 'https://e0.pxfuel.com/wallpapers/388/1010/desktop-wallpaper-boxing-ring-inside-boxing-ring.jpg',
-      name: 'Boxing Ring'
     },
+    // {
+    //   url: 'https://media.istockphoto.com/id/655781960/photo/determined-male-working-out-in-gym-lifting-weights.jpg?s=612x612&w=0&k=20&c=k9C0fgbVHYxiQLe8hckSx9OqAEaGVUfiVXYq8JGxhoE=',
+    //   name: 'lio'
+    // },
+    // {
+    //   url: 'https://media.istockphoto.com/id/655781960/photo/determined-male-working-out-in-gym-lifting-weights.jpg?s=612x612&w=0&k=20&c=k9C0fgbVHYxiQLe8hckSx9OqAEaGVUfiVXYq8JGxhoE=',
+    //   name: 'lio'
+    // },
+    // {
+    //   url: 'https://media.istockphoto.com/id/655781960/photo/determined-male-working-out-in-gym-lifting-weights.jpg?s=612x612&w=0&k=20&c=k9C0fgbVHYxiQLe8hckSx9OqAEaGVUfiVXYq8JGxhoE=',
+    //   name: 'lio'
+    // },
     {
       url: 'https://media.istockphoto.com/id/655781960/photo/determined-male-working-out-in-gym-lifting-weights.jpg?s=612x612&w=0&k=20&c=k9C0fgbVHYxiQLe8hckSx9OqAEaGVUfiVXYq8JGxhoE=',
       name: 'lio'
@@ -53,17 +55,14 @@ const Training = () => {
     <div className={Style.container}>
       <Navbar />
       <div className={Style.carousel}>
-        <button className={Style.changeContent} onClick={handlePrevious}><FaLessThan /></button>
+        <button onClick={handlePrevious}><FaLessThan /></button>
         <div className={Style.imgContainer}>
-          <img src={photos[index].url} alt={photos[index].name} className={Style.img}/>
+          <img src={photos[index].url} alt={photos[index].name} />
           <p>{photos[index].name}</p>
         </div>
-        <button className={Style.changeContent} onClick={handleNext}><FaGreaterThan /></button>
+        <button onClick={handleNext}><FaGreaterThan /></button>
       </div>
-      <div className={Style.footer}>
-    <Footer/>
-      </div>
-
+      <Footer />
     </div>
   );
 };
