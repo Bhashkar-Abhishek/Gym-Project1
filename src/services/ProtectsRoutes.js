d9f91d1ab1ea9cc69a2e454d212e7f5243654718
AmanDsan <126654741+AmanDsan@users.noreply.github.com>
2023-04-21 13:31:30 +0530

    Update ProtectsRoutes.js

--- src/services/ProtectsRoutes.js
+++ src/services/ProtectsRoutes.js
@@ -4,7 +4,7 @@ import {Outlet, Navigate} from 'react-router-dom'
 export default function ProtectsRoutes() {
     const isLogin = localStorage.getItem('loggedin')
   return (
-       isLogin ? <Outlet/> : <Navigate to='/' />
+       isLogin ? <Navigate to='/' /> : <Outlet/> 
   )
   
 }
